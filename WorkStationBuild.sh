e79d5e90cd223e017cc0a1cf25e30f9f2b92edae
Paul Macdonnell <pgmac@pgmac.net>
2016-06-12 22:26:45 +1000

    Upgrade hipchat

--- WorkStationBuild.sh
+++ WorkStationBuild.sh
@@ -187,10 +187,16 @@ ssh_config() {
 }
 
 hipchat() {
-	echo "deb http://downloads.hipchat.com/linux/apt stable main" > /etc/apt/sources.list.d/atlassian-hipchat.list
-	wget -O - https://www.hipchat.com/keys/hipchat-linux.key | apt-key add -
-	apt-get update
-	apt-get install hipchat
+	# Previous hipchat version
+	#echo "deb http://downloads.hipchat.com/linux/apt stable main" > /etc/apt/sources.list.d/atlassian-hipchat.list
+	#wget -O - https://www.hipchat.com/keys/hipchat-linux.key | apt-key add -
+	#apt-get update
+	#apt-get install hipchat
+	# HipChat 4
+	sh -c 'echo "deb https://atlassian.artifactoryonline.com/atlassian/hipchat-apt-client $(lsb_release -c -s) main" > /etc/apt/sources.list.d/atlassian-hipchat4.list'
+	wget -O - https://atlassian.artifactoryonline.com/atlassian/api/gpg/key/public | sudo apt-key add -
+	sudo apt-get update
+	sudo apt-get install hipchat4
 }
 
 if [ $# -eq 0 ]
